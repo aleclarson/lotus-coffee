8e75da9c0aab2a26dfeab2eda8e98fd6b20b1c6b
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-27 04:27:27 -0700

    08/27/2016 4:27 AM

--- js/transform.js
+++ js/transform.js
@@ -1,6 +1,6 @@
-var Promise, coffee, fs, log, path, printLocation, printOffender, repeatString;
+var Promise, coffee, fs, log, path, printSyntaxError;
 
-repeatString = require("repeat-string");
+printSyntaxError = require("printSyntaxError");
 
 Promise = require("Promise");
 
@@ -52,7 +52,9 @@ module.exports = Promise.wrap(function(file, options) {
     });
   }).fail(function(error) {
     if (error instanceof SyntaxError) {
-      error.print = SyntaxError.Printer(error, file.path);
+      error.print = function() {
+        return printSyntaxError(error, file.path);
+      };
     }
     throw error;
   }).then(function(compiled) {
@@ -70,69 +72,5 @@ module.exports = Promise.wrap(function(file, options) {
     }
   });
 });
-
-SyntaxError.Printer = function(error, filePath) {
-  return function() {
-    var code, column, label, line, message;
-    label = log.color.red(error.constructor.name);
-    message = error.message;
-    line = error.location.first_line;
-    code = error.code.split(log.ln);
-    column = error.location.first_column;
-    log.plusIndent(2);
-    log.moat(1);
-    log.withLabel(label, message);
-    log.moat(1);
-    printLocation(line - 1, filePath);
-    log.moat(1);
-    printOffender(code[line], column);
-    return log.popIndent();
-  };
-};
-
-printLocation = function(lineNumber, filePath, funcName) {
-  var dirName, dirPath;
-  log.moat(0);
-  log.yellow("" + lineNumber);
-  log(repeatString(" ", 5 - ("" + lineNumber).length));
-  if (filePath != null) {
-    dirName = path.dirname(filePath);
-    dirPath = path.relative(lotus.path, dirName);
-    if (dirName !== ".") {
-      log.green.dim(dirPath + "/");
-    }
-    log.green(path.basename(filePath));
-  }
-  if (funcName != null) {
-    if (filePath != null) {
-      log(" ");
-    }
-    log.cyan.dim("within");
-    log(" ");
-    log.cyan(funcName);
-  }
-  return log.moat(0);
-};
-
-printOffender = function(line, column) {
-  var columnIndent, hasOverflow, rawLength;
-  rawLength = line.length;
-  line = line.replace(/^\s*/, "");
-  columnIndent = repeatString(" ", column + line.length - rawLength);
-  log.pushIndent(log.indent + 5);
-  hasOverflow = (log.process != null) && log.process.stdout.isTTY && log.indent + line.length > log.process.stdout.columns;
-  if (hasOverflow) {
-    line = line.slice(0, log.process.stdout.columns - log.indent - 4);
-  }
-  log.moat(0);
-  log(line);
-  if (hasOverflow) {
-    log.gray.dim("...");
-  }
-  log(log.ln, columnIndent);
-  log.red("▲");
-  log.moat(0);
-  return log.popIndent();
-};
 
 //# sourceMappingURL=map/transform.map
--- src/transform.coffee
+++ src/transform.coffee
@@ -1,5 +1,5 @@
 
-repeatString = require "repeat-string"
+printSyntaxError = require "printSyntaxError"
 Promise = require "Promise"
 coffee = require "coffee-script"
 path = require "path"
@@ -44,7 +44,8 @@ module.exports = Promise.wrap (file, options) ->
 
   .fail (error) ->
     if error instanceof SyntaxError
-      error.print = SyntaxError.Printer error, file.path
+      error.print = ->
+        printSyntaxError error, file.path
     throw error
 
   .then (compiled) ->
@@ -66,68 +67,3 @@ module.exports = Promise.wrap (file, options) ->
     if sourceMap
       file.mapDest = mapPath
       fs.write mapPath, compiled.v3SourceMap
-
-SyntaxError.Printer = (error, filePath) -> () ->
-
-  label = log.color.red error.constructor.name
-  message = error.message
-  line = error.location.first_line
-  code = error.code.split log.ln
-  column = error.location.first_column
-
-  log.plusIndent 2
-  log.moat 1
-  log.withLabel label, message
-  log.moat 1
-  printLocation line - 1, filePath
-  log.moat 1
-  printOffender code[line], column
-  log.popIndent()
-
-printLocation = (lineNumber, filePath, funcName) ->
-
-  log.moat 0
-
-  log.yellow "#{lineNumber}"
-  log repeatString " ", 5 - "#{lineNumber}".length
-
-  if filePath?
-    dirName = path.dirname filePath
-    dirPath = path.relative lotus.path, dirName
-    log.green.dim dirPath + "/" if dirName isnt "."
-    log.green path.basename filePath
-
-  if funcName?
-    log " " if filePath?
-    log.cyan.dim "within"
-    log " "
-    log.cyan funcName
-
-  log.moat 0
-
-printOffender = (line, column) ->
-
-  rawLength = line.length
-
-  # Remove spaces from the beginning of the offending line of code.
-  line = line.replace /^\s*/, ""
-
-  # Calculate the position of the ▲ icon.
-  columnIndent = repeatString " ", column + line.length - rawLength
-
-  log.pushIndent log.indent + 5
-
-  hasOverflow = log.process? and
-                log.process.stdout.isTTY and
-                log.indent + line.length > log.process.stdout.columns
-
-  if hasOverflow
-    line = line.slice 0, log.process.stdout.columns - log.indent - 4
-
-  log.moat 0
-  log line
-  log.gray.dim "..." if hasOverflow
-  log log.ln, columnIndent
-  log.red "▲"
-  log.moat 0
-  log.popIndent()
